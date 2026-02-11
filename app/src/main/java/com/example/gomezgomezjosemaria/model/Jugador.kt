79dbbb20f1ceb0c17049d77048ffad22fc10a6eb
Jose <josegomezzmlg@gmail.com>
2026-02-11 16:38:14 +0100

    Segundo commit

---
+++ app/src/main/java/com/example/gomezgomezjosemaria/model/Jugador.kt
@@ -0,0 +1,9 @@
+package com.example.gomezgomezjosemaria.model
+
+data class jugador(
+    val id: String = "",
+    val nombre: String = "",
+    val numero: Double = 0.0,
+    val posicion: String,
+    val descripcion: String = ""
+)
