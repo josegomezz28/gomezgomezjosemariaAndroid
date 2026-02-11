fcd7327e17b61ab4723668e640127328a1c6afeb
Jose <josegomezzmlg@gmail.com>
2026-02-11 18:54:50 +0100

    Quinto commit

--- app/src/main/java/com/example/gomezgomezjosemaria/pantallas/PantallaHome.kt
+++ app/src/main/java/com/example/gomezgomezjosemaria/pantallas/PantallaHome.kt
@@ -1,15 +1,21 @@
 package com.example.gomezgomezjosemaria.pantallas
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -18,6 +24,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -27,9 +34,7 @@ import com.example.gomezgomezjosemaria.View.JugadoresCard
 @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun PantallaHome(
-        email: String,
-        onLogout: () -> Unit,
-        onViewProducto: (id: String, nombre: String, numero: Double,nacionalidad: String,posicion:String) -> Unit,
+        onViewJugadores: (id: String, nombre: String, numero: Double,nacionalidad: String,posicion:String) -> Unit,
         viewModel: ViewModel = viewModel()
     ) {
 
@@ -39,7 +44,6 @@ import com.example.gomezgomezjosemaria.View.JugadoresCard
     var numero by remember { mutableStateOf("") }
     var nacionalidad by remember { mutableStateOf("") }
     var posicion by remember { mutableStateOf("") }
-    var editandoId by remember { mutableStateOf<String?>(null) }
     var error by remember { mutableStateOf<String?>(null) }
 
 
@@ -49,19 +53,26 @@ import com.example.gomezgomezjosemaria.View.JugadoresCard
         TopAppBar(
             title = { Text("Plantilla Temporada 25/26") },
         )
-        Spacer(Modifier.height(16.dp))
+        Spacer(Modifier.height(11.dp))
 
         LazyColumn {
-            items(jugadores) { jug ->
-                JugadoresCard(
-                    jugadores = jug,
-                    onView = {
-                        onViewJugador(jug.id, jug.nombre, jug.numero,jug.nacionalidad,jug.posicion)
-                    },
-                )
-                Spacer(Modifier.height(8.dp))
+
+            Card(
+                modifier = Modifier.fillMaxWidth(),
+                elevation = CardDefaults.cardElevation(5.dp)
+            ) {
+                Row(
+                    modifier = Modifier.padding(12.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Column(modifier = Modifier.weight(3f)) {
+                        Text(jugadores.nombre, style = MaterialTheme.typography.titleMedium)
+                       // Int(jugadores.numero,style=MaterialTheme.typography.bodyMedium)
+                        Text(jugadores.nacionalidad, style = MaterialTheme.typography.bodyMedium)
+                        Text(jugadores.posicion, style = MaterialTheme.typography.bodyMedium)
+                    }
+                }
             }
         }
-
     }
 }
